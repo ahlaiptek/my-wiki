abe68b6d1935a5dda9a54dd361295c2d6661564f
ahlaiptek <iptekahla@gmail.com>
2024-06-24 07:01:43 +0700

    The first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="test">
+        <div className="box"></div>
+      </div>
+      <h1>Hello World</h1>
+    </Fragment>
+  );
+}
+
+export default App;
